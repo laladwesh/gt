0bd15d0ab613bab45f0431f432b5d3a3e667b678
laladwesh <guptaavinash302@gmail.com>
2026-02-27 18:17:00 +0530

    chore: fix dependencies : src/App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Academics from './pages/Academics';
+import Publications from './pages/Publications';
+import Students from './pages/Students';
+import Projects from './pages/Projects';
+import Download from './pages/Download';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename="/trivedi">
+      <div className="flex flex-col min-h-screen bg-gray-50">
+        <Navbar />
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="academics" element={<Academics />} />
+            <Route path="publications" element={<Publications />} />
+            <Route path="students" element={<Students />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="download" element={<Download />} />
+            <Route path="contact" element={<Contact />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
